ee1b4156ab0d3c0d65a495e3aa541d490e1872d2
Monita1234 <nayibemontealegre@hotmail.com>
2015-07-10 15:57:12 -0500

    setings
    basededatos

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -9,18 +9,18 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': 'db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        #'USER': '',
-        #'PASSWORD': '',
-        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
-    #}
-#}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'd1gg0ptimpb8qp',                      # Or path to database file if using sqlite3.
+         The following settings are not used with sqlite3:
+        'USER': 'qnznqdkvipvycm',
+        'PASSWORD': 'KmnH2GXAaQKq3W5_bO0h8SR5Pb',
+        'HOST': 'ec2-54-204-26-8.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
+    }
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -32,7 +32,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
-
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
@@ -142,7 +142,7 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'reportlab',
     'gunicorn',
-    #'psycopg2',
+    'psycopg2',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
